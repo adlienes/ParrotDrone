8aa06237ec15ea25c3144783aedef13825676da1
Ahmet Enes ADLIĞ <adli_enes@hotmail.com>
2018-03-27 21:05:44 +0300

    Yol Çizdirme Yapıldı
    Yol Çizdirme Yapıldı

--- app/src/main/java/com/example/enes/parrotdrone/GridActivity.java
+++ app/src/main/java/com/example/enes/parrotdrone/GridActivity.java
@@ -5,13 +5,19 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.Console;
 
 public class GridActivity extends AppCompatActivity {
 
     Button yukari,asagi,sol,sag,donme;
     Dialog yukaripopup,asagipopup,solpopup,sagpopup,donmepopup;
+    EditText EtYukari,EtYukari2,EtYukari3,EtAsagi,EtSol,EtSag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,12 @@ public class GridActivity extends AppCompatActivity {
         sol=findViewById(R.id.sol);
         sag=findViewById(R.id.sag);
         donme=findViewById(R.id.donme);
+
+        EtYukari=findViewById(R.id.Etyukari);
+        EtYukari2=findViewById(R.id.EtYukari2);
+        EtYukari3=findViewById(R.id.EtYukari3);
+        EtSol=findViewById(R.id.EtSol);
+        EtSag=findViewById(R.id.Etsag);
 
         yukaripopup=new Dialog(this);
         asagipopup=new Dialog(this);
@@ -38,11 +50,33 @@ public class GridActivity extends AppCompatActivity {
                 yukaripopup.setContentView(R.layout.yukaripopup);
 
                 Button btntamam;
+                final EditText popupgiris;
+                popupgiris=yukaripopup.findViewById(R.id.editText);
                 btntamam=yukaripopup.findViewById(R.id.btnbaglan);
                 btntamam.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        yukaripopup.dismiss();
+
+                        if(popupgiris.getText().toString().equals("5"))
+                        {
+                            EtYukari.setVisibility(View.VISIBLE);
+                            yukaripopup.dismiss();
+                        }
+                        else if(popupgiris.getText().toString().equals("4"))
+                        {
+                            EtYukari2.setVisibility(View.VISIBLE);
+                            yukaripopup.dismiss();
+                        }
+                        else if (popupgiris.getText().toString().equals("6"))
+                        {
+                            EtYukari3.setVisibility(View.VISIBLE);
+                            yukaripopup.dismiss();
+                        }
+                        else {
+                            Toast.makeText(GridActivity.this, "Yanlış Değer Girdiniz.Tekrar Deneyin.", Toast.LENGTH_SHORT).show();
+                        }
+
+
                     }
                 });
                 yukaripopup.show();
@@ -55,6 +89,8 @@ public class GridActivity extends AppCompatActivity {
                 asagipopup.setContentView(R.layout.asagipopup);
 
                 Button btntamam;
+                final EditText popupgiris;
+                popupgiris=asagipopup.findViewById(R.id.editText);
                 btntamam=asagipopup.findViewById(R.id.btnbaglan);
                 btntamam.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -62,7 +98,6 @@ public class GridActivity extends AppCompatActivity {
                         asagipopup.dismiss();
                     }
                 });
-                asagipopup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 asagipopup.show();
             }
         });
@@ -73,14 +108,28 @@ public class GridActivity extends AppCompatActivity {
                 solpopup.setContentView(R.layout.solpopup);
 
                 Button btntamam;
+                final EditText popupgiris;
+                popupgiris=solpopup.findViewById(R.id.editText);
                 btntamam=solpopup.findViewById(R.id.btnbaglan);
                 btntamam.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        solpopup.dismiss();
+
+
+                        String log=popupgiris.getText().toString();
+                        Log.w(log,"asdsd");
+                        if(popupgiris.getText().toString().equals("2"))
+                        {
+                            EtSol.setVisibility(View.VISIBLE);
+                            solpopup.dismiss();
+                        }
+                        else {
+                            Toast.makeText(GridActivity.this, "Yanlış Değer Girdiniz.Tekrar Deneyin.", Toast.LENGTH_SHORT).show();
+                        }
+
+
                     }
                 });
-                solpopup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 solpopup.show();
             }
         });
@@ -91,14 +140,25 @@ public class GridActivity extends AppCompatActivity {
                 sagpopup.setContentView(R.layout.sagpopup);
 
                 Button btntamam;
+                final EditText popupgiris;
+                popupgiris=sagpopup.findViewById(R.id.editText);
                 btntamam=sagpopup.findViewById(R.id.btnbaglan);
                 btntamam.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        sagpopup.dismiss();
+
+                        if(popupgiris.getText().toString().equals("3"))
+                        {
+                            EtSag.setVisibility(View.VISIBLE);
+                            sagpopup.dismiss();
+                        }
+                        else
+                        {
+                            Toast.makeText(GridActivity.this, "Yanlış Değer Girdiniz.Tekrar Deneyin.", Toast.LENGTH_SHORT).show();
+                        }
+
                     }
                 });
-                sagpopup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 sagpopup.show();
             }
         });
@@ -116,7 +176,6 @@ public class GridActivity extends AppCompatActivity {
                         donmepopup.dismiss();
                     }
                 });
-                donmepopup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 donmepopup.show();
             }
         });
